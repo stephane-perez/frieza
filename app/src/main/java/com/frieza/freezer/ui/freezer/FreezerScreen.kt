719a84c8a421c64996ef71378275e9a244b73509
Strider <perez.steph.pro@gmail.com>
2026-09-20 00:42:40 +0200

    Compilation error due to bad import

--- app/src/main/java/com/frieza/freezer/ui/freezer/FreezerScreen.kt
+++ app/src/main/java/com/frieza/freezer/ui/freezer/FreezerScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.stickyHeader
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
